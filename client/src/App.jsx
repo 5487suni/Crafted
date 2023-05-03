462acacca5d01d1380119c967c658c11db811ea7
Sanchali Sharma <sharmasanchali06@gmail.com>
2023-05-03 14:04:21 +0530

    created routes for login
    created routes for login component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {Login} from "./Routes.js"
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/login' element={<Login/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
