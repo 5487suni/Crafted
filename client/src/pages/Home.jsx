364d0a1ea7a51ec3c4cc95873a0df9f4540b8b09
5487suni <97465702+5487suni@users.noreply.github.com>
2023-05-09 12:12:11 +0530

    added files

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
 import Header from "../components/layout/Header";
+import Hero from "../components/route/Hero/Hero";
 
 const Home = () => {
   return (
     <div>
         <Header activeHeading={1}/>
+        <Hero />
     </div>
   )
 }
 
-export default Home
+export default Home;
