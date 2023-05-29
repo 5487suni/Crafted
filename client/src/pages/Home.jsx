6ee96d37c1c441958140b43a3db293a6c426d2cf
5487suni <97465702+5487suni@users.noreply.github.com>
2023-05-29 17:35:14 +0530

    added folders for home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from "../components/layout/Header";
 import Hero from "../components/route/Hero/Hero";
-
+import Categories from '../components/route/Categories/Categories';
+import BestDeals from '../components/route/BestDeals/BestDeals';
 const Home = () => {
   return (
     <div>
         <Header activeHeading={1}/>
         <Hero />
+        <Categories />
+        <BestDeals />
     </div>
   )
 }
