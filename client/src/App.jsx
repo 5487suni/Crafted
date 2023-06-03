c13d083dae24e1b4a3b20c6d7a50acc8e90e6315
Sanchali Sharma <sharmasanchali06@gmail.com>
2023-06-03 11:12:53 +0530

    frontend user login
    user activation url and activation page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import {Login,Home} from "./Routes.js"
+import {Login,Home,ActivationPage} from "./Routes.js"
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home/>}/>
+      <Route path="/activation/:activation_token" element={<ActivationPage/>}/>
       <Route path='/login' element={<Login/>}/>
     </Routes>
+    <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </BrowserRouter>
   )
 }
