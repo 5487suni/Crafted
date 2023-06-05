ab1b10d6b332cc282ef982c1444319366e54babd
5487suni <97465702+5487suni@users.noreply.github.com>
2023-06-06 00:42:56 +0530

    bug fixed done

--- client/src/components/route/ProductDetailsCard/ProductDetailsCard.jsx
+++ client/src/components/route/ProductDetailsCard/ProductDetailsCard.jsx
@@ -23,13 +23,13 @@ const ProductDetailsCard = ({ setOpen, data }) => {
             {
                 data ? (
                     <div className="z-50 fixed w-full h-screen top-0 left-0 bg-[#00000030] z-40 flex items-center justify-center">
-                        <div className="z-50 w-[75%] 800px:w-[60%] h-[90vh] overflow-y-scroll 800px:h-[75vh] bg-white rounded-md shadow-sm relative p-4">
+                        <div className="z-50 w-[85%] 800px:w-[60%] h-[90vh] overflow-y-scroll 800px:h-[75vh] bg-white rounded-md shadow-sm relative p-4">
                             <RxCross1
                                 size={30}
                                 className="absolute right-3 top-3 z-50"
                                 onClick={() => setOpen(false)}
                             />
-                            {<div className="z-50 block w-full 800px:flex">
+                            {<div className="z-50 block w-full 800px:flex flex">
                                 <div className="w-full 800px:w-[50%]">
                                     <img src={data.image_Url[0].url} alt="" />
                                     <div className="flex">
@@ -53,13 +53,13 @@ const ProductDetailsCard = ({ setOpen, data }) => {
                                     </div>
                                     <h5 className="text-[16px] text-[red] mt-5">({data.total_sell}) Sold out</h5>
                                 </div>
-                                <div className="w-full 800px:w-[50%] pt-5 pl-[5px] pr-[5px]">
+                                <div className="w-full 800px:w-[50%] pt-5 pl-[5px] pr-[5px] ">
                                     <h1 className={`${styles.productTitle} text-[20px]`}>
                                         {data.name}
                                     </h1>
                                     <p>{data.description}</p>
                                     <div className="flex pt-3"> <h4 className={`${styles.productDiscountPrice}`}>
-                                        {data.discount_Price}$
+                                        {data.discount_price}$
                                     </h4>
                                         <h3 className={`${styles.price}`}>
                                             {data.Price ? data.Price + "$" : null}
