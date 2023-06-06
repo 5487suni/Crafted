8a9bfbed1258b14c476fbf545f18cb2b7c5ad71a
Aditi Shandilya <122087467+aditishandily@users.noreply.github.com>
2023-06-06 08:50:27 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React,{useEffect} from 'react'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import {Login,Home,ActivationPage,ProductsPage,BestSellingPage,EventsPage,FAQPage} from "./Routes.js"
+import {Login,Home,ActivationPage,ProductsPage,BestSellingPage,EventsPage,FAQPage } from "./Routes.js"
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Store from './redux/store'
