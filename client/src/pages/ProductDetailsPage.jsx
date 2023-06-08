218315dac468c6d74a6be937992c240218163fa8
Aditi Shandilya <122087467+aditishandily@users.noreply.github.com>
2023-06-08 22:15:13 +0530

    create product detalis

--- client/src/pages/ProductDetailsPage.jsx
+++ client/src/pages/ProductDetailsPage.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
-import ProductDetails from "../components/Products/ProductDetails";
+import ProductDetails from "../components/Products/ProductDetails.jsx";
+import { productData } from '../static/data';
 
 const ProductDetailsPage = () => {
     const { name } = useParams();
-    const [data, setData] = useState(null);
+    const [data,setData] = useState(null);
     const productName = name.replace(/-/g, " ");
 
     useEffect(() => {
