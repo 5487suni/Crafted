465de909558566a601ac262b7ef102582663166b
5487suni <97465702+5487suni@users.noreply.github.com>
2023-06-08 17:16:23 +0530

    updated files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -30,7 +30,7 @@ const App = () => {
         <Route path="/activation/:activation_token" element={<ActivationPage />} />
         <Route path='/login' element={<Login />} />
         <Route path="/products" element={<ProductsPage />} />
-        <Route path="/product/:id" element={<ProductDetailsPage />} />
+        <Route path="/product/:name" element={<ProductDetailsPage />} />
         <Route path="/best-selling" element={<BestSellingPage />} />
         <Route path="/events" element={<EventsPage />} />
         <Route path="/faq" element={<FAQPage />} />
