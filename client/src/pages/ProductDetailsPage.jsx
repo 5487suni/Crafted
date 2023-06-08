c08a2f1eebd50d82d8ee558af42329d8ff8d9771
Aditi Shandilya <122087467+aditishandily@users.noreply.github.com>
2023-06-08 17:19:19 +0530

    update

--- client/src/pages/ProductDetailsPage.jsx
+++ client/src/pages/ProductDetailsPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ProductDetails from "../components/Products/ProductDetails";
+import Header from '../components/layout/Header';
 
 const ProductDetailsPage = () => {
     return(
